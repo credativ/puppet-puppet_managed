10502788a9c729c054b525d497f54d34520214a6
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2016-04-14 14:04:12 +0200

    Refactor/Feature completion/First releasable version
    This commit includes the following changes:
    - Some refactoring to make the method more readable
    - Add support for a custom prefix (no support for custom block
      commenters so far, but this can be done in a later version)
    - Add support for a verbose flag that allows to show class and
      template filename (although latter needs to be passed)

--- lib/puppet/parser/functions/puppet_managed.rb
+++ lib/puppet/parser/functions/puppet_managed.rb
@@ -1,49 +1,73 @@
 module Puppet::Parser::Functions
   newfunction(:puppet_managed, :type => :rvalue) do |args|
     comment_methods = {
-      'hash'    => { :prefix => '#' },
-      'c-block' => {
-        :block_start => '/*',
-        :block_end => '  */',
+      'hash'        => { :prefix => '#' },
+      'sql'         => { :prefix => '--' },
+      'percent'     => { :prefix => '%' },
+      'vim'         => { :prefix => '"' },
+
+
+      'xml'         => {
+        :block_start        => '<!--',
+        :block_end          => '-->',
+        :block_continuation => ''
+      },
+
+      'c-block'     => {
+        :block_start        => '/*',
+        :block_end          => '  */',
         :block_continuation => '  *'
       },
     }
-    options    = {
+    opts    = {
+      'filename'        => nil,
       'comment_style'   => 'hash',
-      'add_module_name' => false,
+      'verbose'         => false,
+      'prefix'          => nil,
     }
-    managed_text    = ['MANAGED BY PUPPET']
 
-    if options['add_module_name']
-      managed_text.push("Module: #{scope.to_hash['module_name']}")
-    end
+    # The default managed by puppet text
+    managed_text    = ['MANAGED BY PUPPET']
+    prefix          = ''
+    block_comment   = false
 
     unless args.empty?
-      user_provided_options = args.shift
-      options.merge!(user_provided_options)
+      user_provided_opts = args.shift
+      opts.merge!(user_provided_opts)
     end
-    #opts = default_opts
 
-    #opts = {}
-
-
-    comment_style = options['comment_style']
+    comment_style = opts['comment_style']
     unless comment_methods.include?(comment_style)
-      raise Puppet::ParseError, "puppet_managed: passed unknown comment_style (#{opts{'comment_style'}})"
+      raise Puppet::ParseError, "puppet_managed: passed unknown comment_style (#{opts['comment_style']})"
+    end
+    comment_method = comment_methods[comment_style]
+
+    if ! opts['prefix'].nil?
+      debug("comment_style overriden, because prefix is in args")
+      prefix = opts['prefix']
+    elsif comment_method.has_key?(:prefix)
+      block_comment = false
+      prefix        = comment_method[:prefix]
+    elsif comment_method.has_key?(:block_start)
+      block_comment = true
     end
 
-    if comment_methods[comment_style].has_key?(:prefix)
-        prefix = comment_methods[comment_style][:prefix]
-        managed_text.map! { |line| prefix + " " + line }
+
+    if opts['verbose']
+      managed_text.unshift('')
+      managed_text.push('')
+      managed_text.push("Class: #{lookupvar('name')}")
+      managed_text.push("Path: #{opts['filename']}")
+      managed_text.push('')
     end
 
-    if comment_methods[comment_style].has_key?(:block_start)
-      block_start = comment_methods[comment_style][:block_start]
-      block_end = comment_methods[comment_style][:block_end]
+    if block_comment
+      block_start   = comment_method[:block_start]
+      block_end     = comment_method[:block_end]
 
-      # Start with line continuation if we have one
+      # Start with block continuation if we have one
       # (neccessary to do first, to avoid double prefixing)
-      if comment_methods[comment_style].has_key?(:block_continuation)
+      if comment_method.has_key?(:block_continuation)
         block_continuation = comment_methods[comment_style][:block_continuation]
         managed_text.map! { |line| block_continuation + " " + line }
       end
@@ -52,6 +76,8 @@ module Puppet::Parser::Functions
       managed_text.unshift(block_start)
       managed_text.push(block_end)
 
+    else
+      managed_text.map! { |line| prefix + " " + line }
     end
 
     managed_text.map! { |item| item + "\n" }.join("")
--- spec/functions/puppet_managed_spec.rb
+++ spec/functions/puppet_managed_spec.rb
@@ -7,10 +7,38 @@ describe 'puppet_managed' do
         .and_return ("# MANAGED BY PUPPET\n")
     }
   end
+
+
   context 'with comment_style => c-block' do
     it {
       is_expected.to run.with_params({ 'comment_style' => 'c-block'})
         .and_return ("/*\n  * MANAGED BY PUPPET\n  */\n")
+    }
+  end
+ context 'with comment_style => xml' do
+    it {
+      is_expected.to run.with_params({ 'comment_style' => 'xml'})
+        .and_return ("<!--\n MANAGED BY PUPPET\n-->\n")
+    }
+  end
+  context 'with comment_style => sql' do
+    it {
+      is_expected.to run.with_params({ 'comment_style' => 'sql'})
+        .and_return("-- MANAGED BY PUPPET\n")
+    }
+  end
+
+  context 'with comment_style => vim' do
+    it {
+      is_expected.to run.with_params({ 'comment_style' => 'vim'})
+        .and_return("\" MANAGED BY PUPPET\n")
+    }
+  end
+
+  context 'with custom prefix (!!)' do
+    it {
+      is_expected.to run.with_params({ 'prefix' => '!!'})
+        .and_return("!! MANAGED BY PUPPET\n")
     }
   end
 end
